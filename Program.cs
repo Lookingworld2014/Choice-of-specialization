abfa19a5cccf2e6e5cfa0f8dd1857bfec5749425
Nastya <nas.jiltsowa2010@gmail.com>
2023-03-23 22:51:45 +0300

    second

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
 ﻿//Программа, которая из имеющегося массива строк формирует массив строк, длина которых меньше, либо равна 3 символа.
+
+
+
+int [] arr = GetArray();
+
+int[] GetArray()
+{
+    int[] arr = new int[3];
+    for (int i = 0; i < arr.Length; i++)
+        arr[i] = new Random().Next(1, 10);
+    return arr;
+
+    Console.WriteLine(arr);
+
+}
+
