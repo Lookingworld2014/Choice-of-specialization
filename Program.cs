a8c54a19371a97c7fff83186cea56f2b78249d6a
Nastya <nas.jiltsowa2010@gmail.com>
2023-03-24 00:01:31 +0300

    2300

--- Program.cs
+++ Program.cs
@@ -2,16 +2,59 @@
 
 
 
-int [] arr = GetArray();
+int[] arr = GetArray();
+
+PrintArray(arr);
+
+RemakeArray(arr);
+
+int[] array = PrintRemake();
 
 int[] GetArray()
 {
     int[] arr = new int[3];
     for (int i = 0; i < arr.Length; i++)
+    {
         arr[i] = new Random().Next(1, 10);
-    return arr;
 
-    Console.WriteLine(arr);
+    }
+    return arr;
 
 }
 
+void PrintArray(int[] arr)
+
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+
+
+}
+
+int [] RemakeArray(int[] arr)
+{
+    
+array = new int [3];
+
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if(array[i] < 3)
+    count++;
+
+}
+return array;
+}
+
+void PrintRemake(int[]array)
+
+{
+for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i] + " ");
+    }
+
+}
